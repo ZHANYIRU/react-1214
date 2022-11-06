127b51d568617ef1a5376be51eaf14981732cfdd
YI-RU <game665987@gmail.com>
2022-11-07 01:31:31 +0800

    okorder

--- src/pages/Cart/Cart.js
+++ src/pages/Cart/Cart.js
@@ -1,11 +1,13 @@
 import styled from '../../styles/Cart.module.scss'
 import CartTitle from './components/CartTitle'
 import BuyCart from './components/BuyCart'
+import OkOrder from './components/OkOrder'
 function Cart() {
   return (
     <>
       <div className={styled.body}>
         <CartTitle />
+        <OkOrder />
         {/* <BuyCart /> */}
       </div>
     </>
