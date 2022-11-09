8a0faed119f8effa86c906c6fd7e5a8a6f7b6c31
YI-RU <game665987@gmail.com>
2022-11-09 18:26:22 +0800

    123

--- src/pages/Cart/child-pages/CartDetail.js
+++ src/pages/Cart/child-pages/CartDetail.js
@@ -1,11 +1,6 @@
-import { useState } from 'react'
 import styled from '../../../styles/cart-scss/cartDetail.module.scss'
 function CartDetail(props) {
-  const [total, setTotal] = useState(0)
-  return <div className={styled.dtWrap}>購物車詳細{total}
-    <button onClick={() => {
-      setTotal(total + 1)
-    }}>+</button></div>
+  return <div className={styled.dtWrap}>購物車詳細</div>
 }
 
 export default CartDetail
