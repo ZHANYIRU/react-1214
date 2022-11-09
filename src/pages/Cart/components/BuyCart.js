6e6d9422a0422c11eb921dcd3e74baa83b280d53
YI-RU <game665987@gmail.com>
2022-11-09 19:24:59 +0800

    123

--- src/pages/Cart/components/BuyCart.js
+++ src/pages/Cart/components/BuyCart.js
@@ -1,11 +1,23 @@
+import { useState } from 'react'
 import styled from '../../../styles/cart-scss/BuyCart.module.scss'
 function BuyCart({ step, setStep }) {
+  const [check, setCheck] = useState(false)
   if (step === 1) {
     return (
       <>
         <div className={styled.buyCart}>
           <div className={styled.wrap}>
-            <button>繼續選購</button>
+            <div>
+              <input type="checkbox" id='check' checked={check}
+                onChange={(e) => {
+                  setCheck(e.target.checked)
+                }} />
+              <label htmlFor="check">全選</label>
+            </div>
+            <p>總訂購數量：9</p>
+            <div className={styled.price}>
+              <span>總金額：</span><p>$19000</p>
+            </div>
             <button
               onClick={() => {
                 setStep(step + 1)
