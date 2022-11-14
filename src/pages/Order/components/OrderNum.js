6ff5d661eabdda8f0ea3e40122c42c3b4bf77abc
YI-RU <game665987@gmail.com>
2022-11-14 22:03:58 +0800

    num

--- src/pages/Order/components/OrderNum.js
+++ src/pages/Order/components/OrderNum.js
@@ -1,7 +1,33 @@
 import React from 'react'
-
+import Accordion from 'react-bootstrap/Accordion'
+import '../../../styles/cart-scss/OrderNum.scss'
 function OrderNum() {
-  return <div>OrderNum</div>
+  return (
+    <Accordion alwaysOpen>
+      <Accordion.Item eventKey="0">
+        <Accordion.Header>Accordion Item #1</Accordion.Header>
+        <Accordion.Body>
+          <div className="div1"></div>
+          <div className="div1"></div>
+          <div className="div1"></div>
+          <div className="div1"></div>
+          <div className="div1"></div>
+        </Accordion.Body>
+      </Accordion.Item>
+      <Accordion.Item eventKey="1">
+        <Accordion.Header>Accordion Item #2</Accordion.Header>
+        <Accordion.Body>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat. Duis aute irure dolor in
+          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+          culpa qui officia deserunt mollit anim id est laborum.
+        </Accordion.Body>
+      </Accordion.Item>
+    </Accordion>
+  )
 }
 
 export default OrderNum
