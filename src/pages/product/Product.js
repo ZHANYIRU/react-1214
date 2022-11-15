6bdf3baf10972ea3d8fb23da928c3dc835b37cb2
nelson <goodgood0331@gmail.com>
2022-11-15 20:18:16 +0800

    1115_zx_20:18

--- src/pages/product/Product.js
+++ src/pages/product/Product.js
@@ -1,5 +1,5 @@
 import { useState, useEffect, useRef } from 'react'
-import { Link, useParams } from 'react-router-dom'
+import { Link, useParams, useLocation } from 'react-router-dom'
 import Slider from './slider'
 import Product_filter from './product_filter'
 // import { Link, useLocation } from 'react-router-dom'
@@ -29,16 +29,37 @@ function Product() {
   const mainHeight = useRef('')
   //附style給filter
   const [fixedd, setFixedd] = useState(false)
+  //偵測是否為手機版面
+  const [mob, setMob] = useState(false)
+
+  //偵測滾動時，filter要吸附的位置
   const scrollFilter = () => {
-    const windowH = window.innerHeight
-    const mainH = mainHeight.current.clientHeight
     const windowScrollY = window.scrollY
-    if (windowScrollY > 770) {
+    let Window_W = window.innerWidth
+    if (windowScrollY > 770 && Window_W > 500) {
       setFixedd(true)
-    } else {
+    } else if (windowScrollY < 770 && Window_W > 500) {
       setFixedd(false)
     }
-    console.log(windowScrollY)
+  }
+  // 切換回product的方法 (讓Bar不要跑掉)
+  const filterLocation = () => {
+    let Window_W = window.innerWidth
+    if (location.pathname === '/product' && Window_W < 500) {
+      setMob(true)
+    } else {
+      setMob(false)
+    }
+  }
+
+  // 視窗寬度方法
+  const gW = () => {
+    let Window_W = window.innerWidth
+    if (Window_W < 500 && location.pathname === '/product') {
+      setMob(true)
+    } else if (Window_W > 500) {
+      setMob(false)
+    }
   }
   //抓取fetch狀態
   const [datas, setDatas] = useState([
@@ -57,9 +78,11 @@ function Product() {
     },
   ])
   const { product_sid } = useParams()
+  const location = useLocation()
 
   const [sid, getSid] = useState('')
 
+  //Fetch所有產品
   const getProductData = async () => {
     const response = await axios.get('http://localhost:3001/product/all')
     const r = response.data
@@ -67,15 +90,17 @@ function Product() {
     setDatas(r)
   }
 
+  //----------------------------
   useEffect(() => {
     getProductData()
+    filterLocation()
+    window.addEventListener('resize', gW)
     window.addEventListener('scroll', scrollFilter)
-  }, [])
+  }, [fixedd, mob])
 
   return (
     <>
       <div className={styled.container}>
-        <div className={styled.filter}></div>
         <div className={styled.empty}></div>
         {/* Slider */}
         <Slider data={data} />
@@ -90,17 +115,20 @@ function Product() {
 
           {/* 種類專區 */}
         </div>
-        <div className={styled.product_nav}>
+        <div className={styled.product_nav} Draggable onDrag={() => {}}>
           <div className={styled.product_nav_box1}>
             <Link>
               <h2>最新上架</h2>
             </Link>
+            {mob ? <p>|</p> : ''}
             <Link>
               <h2>熱門商品</h2>
             </Link>
+            {mob ? <p>|</p> : ''}
             <Link>
               <h2>男女服飾</h2>
             </Link>
+            {mob ? <p>|</p> : ''}
           </div>
           <div className={styled.product_nav_box2}>
             <p>商品類別</p>
@@ -109,9 +137,11 @@ function Product() {
             <Link>
               <h2>專業用品</h2>
             </Link>
+            {mob ? <p>|</p> : ''}
             <Link>
               <h2>飲水用品</h2>
             </Link>
+            {mob ? <p>|</p> : ''}
             <Link>
               <h2>其他配件</h2>
             </Link>
@@ -121,13 +151,16 @@ function Product() {
         {/* 卡片專區 */}
 
         {/* <div className={styled.cardBigBox}> */}
-
+        <Product_filter fixedd={fixedd} mob={mob} setMob={setMob} gW={gW} />
         <div className={styled.cardbox}>
-          <Product_filter fixedd={fixedd} />
           {datas.map((v, i) => {
             return (
-              <Link key={v.product_sid} to={'/product/' + v.product_sid}>
-                <div className={styled.card}>
+              <Link
+                className={styled.card}
+                key={v.product_sid}
+                to={'/product/' + v.product_sid}
+              >
+                <div>
                   <img
                     src="https://cdn1.cybassets.com/media/W1siZiIsIjE2MTQyL3Byb2R1Y3RzLzM2MzA1MjQwLzE2NjM5MDE2NDZfODM4NGYzMjY3ODcxNmYwOGQ3YTUuanBlZyJdLFsicCIsInRodW1iIiwiNjAweDYwMCJdXQ.jpeg?sha=0c0e2037acddca29"
                     alt=""
--- src/pages/product/product_filter.js
+++ src/pages/product/product_filter.js
@@ -3,11 +3,12 @@ import { Link } from 'react-router-dom'
 import React, { useState, useRef, useEffect } from 'react'
 import styled from '../../styles/product-scss/product.module.scss'
 
-export default function Product_filter({ fixedd }) {
+export default function Product_filter({ fixedd, mob, setMob, gW }) {
   const [lowPrice, setLowPrice] = useState('')
   const [highPrice, setHighPrice] = useState('')
   const brandOptions = ['Arcteryx 始祖鳥', 'mmm']
   const [brand, setBrand] = useState('')
+  const [filterOpen, setFilterOpen] = useState(false)
 
   const [datas, setDatas] = useState([
     {
@@ -24,11 +25,9 @@ export default function Product_filter({ fixedd }) {
       size: 'S',
     },
   ])
-  const filterStyle = {
-    position: 'fixed',
-    top: '65px',
-    left: '105px',
-  }
+
+  const mbfilterRef = useRef('')
+  const filterRef = useRef('')
 
   let allProduct = 'http://localhost:3001/product/all'
   let price = 'http://localhost:3001/product/price'
@@ -59,6 +58,17 @@ export default function Product_filter({ fixedd }) {
     }
   }
 
+  // const filterToggle
+  const filterToggle = () => {
+    if (!filterOpen) {
+      mbfilterRef.current.style.height = '500px'
+      setFilterOpen(true)
+    } else {
+      mbfilterRef.current.style.height = '50px'
+      setFilterOpen(false)
+    }
+  }
+
   const filterData = () => {
     if (lowPrice && highPrice && brand) {
       getData(price_brand)
@@ -68,9 +78,13 @@ export default function Product_filter({ fixedd }) {
       getData(brands)
     }
   }
-  useEffect(() => {}, [])
-  return (
-    <div className={fixedd ? `${styled.filter2}` : `${styled.filter}`}>
+
+  // filter樣式 (電腦版)
+  const webFilter = (
+    <div
+      className={fixedd ? `${styled.filter2}` : `${styled.filter}`}
+      ref={filterRef}
+    >
       <form action="">
         <h2>價格</h2>
         <div className={styled.pricebox}>
@@ -99,14 +113,14 @@ export default function Product_filter({ fixedd }) {
         </div>
         <h2> 品牌</h2>
         {/* <input
-          type="text"
-          placeholder="請輸入品牌"
-          name="brands"
-          value={brand}
-          onChange={(e) => {
-            setBrand(e.target.value)
-          }}
-        /> */}
+      type="text"
+      placeholder="請輸入品牌"
+      name="brands"
+      value={brand}
+      onChange={(e) => {
+        setBrand(e.target.value)
+      }}
+    /> */}
         <select
           name="brand"
           id=""
@@ -196,4 +210,132 @@ export default function Product_filter({ fixedd }) {
       </form>
     </div>
   )
+  const mobFilter = (
+    <div className={styled.mbfilter} ref={mbfilterRef}>
+      <form action="">
+        <div className={styled.pricebox}>
+          <input
+            size="5"
+            type="text"
+            placeholder="最低價格"
+            name="lowPrice"
+            value={lowPrice}
+            onChange={(e) => {
+              setLowPrice(e.target.value)
+            }}
+          />
+          <div className={styled.dash}></div>
+          <div className="dash"></div>
+          <input
+            size="5"
+            type="text"
+            placeholder="最高價格"
+            name="highPrice"
+            value={highPrice}
+            onChange={(e) => {
+              setHighPrice(e.target.value)
+            }}
+          />
+          <i class="fa-solid fa-sort"></i>
+          <i
+            class="fa-solid fa-filter"
+            onClick={() => {
+              filterToggle()
+            }}
+          ></i>
+        </div>
+        <h2> 品牌</h2>
+        <select
+          name="brand"
+          id=""
+          value={brand}
+          onChange={(e) => {
+            setBrand(e.target.value)
+          }}
+          className={styled.filterSelect}
+        >
+          <option value="-1">請選出廠牌</option>
+          {brandOptions.map((v, i) => {
+            return (
+              <option key={i} value={i}>
+                {v}
+              </option>
+            )
+          })}
+        </select>
+        <div className={styled.genderRadio}>
+          <div className={styled.genderBox}>
+            <label htmlFor="male">男性</label>
+            <input type="radio" id="male" name="sex" value="male" />
+          </div>
+          <div className={styled.genderBox}>
+            <label htmlFor="female">女性</label>
+            <input type="radio" id="female" name="sex" value="female" />
+          </div>
+        </div>
+        <h2> 防水等級</h2>
+        <div className={styled.checkBoxWrap}>
+          <div className={styled.checkBox}>
+            <input type="checkbox" id="wRes" value="wRes" />
+            <label htmlFor="wRes">抗水（Water Resistant）</label>
+          </div>
+          <div className={styled.checkBox}>
+            <input type="checkbox" id="wRep" value="wRep" />
+            <label htmlFor="wRep">防潑水（Water Repellent）</label>
+          </div>
+          <div className={styled.checkBox}>
+            <input type="checkbox" id="wProof" value="wProof" />
+            <label htmlFor="wProof">防水（Waterproof）</label>
+          </div>
+        </div>
+
+        <button
+          type="submit"
+          onClick={(e) => {
+            e.preventDefault()
+            filterData()
+          }}
+          className={styled.filterButton}
+        >
+          送出
+        </button>
+        <hr />
+        <div className={styled.star}>
+          <Link>
+            <i class="fa-solid fa-star"></i>
+            <i class="fa-solid fa-star"></i>
+            <i class="fa-solid fa-star"></i>
+            <i class="fa-solid fa-star"></i>
+            <i class="fa-solid fa-star"></i>
+          </Link>
+
+          <Link>
+            <i class="fa-solid fa-star"></i>
+            <i class="fa-solid fa-star"></i>
+            <i class="fa-solid fa-star"></i>
+            <i class="fa-solid fa-star"></i>
+          </Link>
+
+          <Link>
+            <i class="fa-solid fa-star"></i>
+            <i class="fa-solid fa-star"></i>
+            <i class="fa-solid fa-star"></i>
+          </Link>
+
+          <Link>
+            <i class="fa-solid fa-star"></i>
+            <i class="fa-solid fa-star"></i>
+          </Link>
+
+          <Link>
+            <i class="fa-solid fa-star"></i>
+          </Link>
+        </div>
+      </form>
+    </div>
+  )
+  useEffect(() => {
+    window.addEventListener('resize', gW)
+  }, [mob])
+  return <>{mob ? mobFilter : webFilter}</>
 }
