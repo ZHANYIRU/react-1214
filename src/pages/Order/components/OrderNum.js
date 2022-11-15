f9e0b6dbeec1014ca2bb13329dd39978b01011fa
YI-RU <game665987@gmail.com>
2022-11-16 02:14:35 +0800

    order

--- src/pages/Order/components/OrderNum.js
+++ src/pages/Order/components/OrderNum.js
@@ -11,16 +11,60 @@ function OrderNum() {
     <>
       <div className={styled.numWrap}>
         <input type="checkbox" id="order" />
-        <label className={styled.orederNum} onClick={openOrder} htmlFor="order">
+        <label className={styled.orderNum} onClick={openOrder} htmlFor="order">
           <p> 訂單編號：0000000000</p>
+          <p>金額：9487</p>
+          <i className="fa-solid fa-chevron-down"></i>
         </label>
-        <div className={open ? `${styled.contentOpen}` : `${styled.content}`}>
-          <div className={styled.div1}>12121</div>
-          <div className={styled.div1}>121212</div>
-          <div className={styled.div1}>21212</div>
-          <div className={styled.div1}>21212</div>
+        <div
+          className={
+            open ? `${styled.contentWrapOpen}` : `${styled.contentWrap}`
+          }
+        >
+          <div className={styled.proContentTitle}>
+            <p>商品</p>
+            <p>單價</p>
+            <p>數量</p>
+            <p>金額</p>
+          </div>
+          <div className={styled.content}>
+            <div className={styled.contentDe}>
+              <div className={styled.imgWrap}>
+                <img
+                  src="https://cdn2.ettoday.net/images/4778/d4778980.jpg"
+                  alt=""
+                />
+              </div>
+              <p>
+                Arcteryx 始祖鳥 Beta LT 女款 Gore Tex登山雨衣/風雨衣 29458
+                鐵克諾紅 Techno
+              </p>
+              <p>600</p>
+              <p>2</p>
+              <p>1200</p>
+            </div>
+            <button>給予評價</button>
+          </div>
+          <div className={styled.content}>
+            <div className={styled.contentDe}>
+              <div className={styled.imgWrap}>
+                <img
+                  src="https://cdn2.ettoday.net/images/4778/d4778980.jpg"
+                  alt=""
+                />
+              </div>
+              <p>
+                Arcteryx 始祖鳥 Beta LT 女款 Gore Tex登山雨衣/風雨衣 29458
+                鐵克諾紅 Techno
+              </p>
+              <p>600</p>
+              <p>2</p>
+              <p>1200</p>
+            </div>
+            <button>給予評價</button>
+          </div>
         </div>
-        <input type="checkbox" id="order1" />
+        {/* <input type="checkbox" id="order1" />
         <label
           className={styled.orederNum}
           onClick={openOrder}
@@ -33,7 +77,7 @@ function OrderNum() {
           <div className={styled.div1}>121212</div>
           <div className={styled.div1}>21212</div>
           <div className={styled.div1}>21212</div>
-        </div>
+        </div> */}
       </div>
       {/* <div className="tabs">
         <div className="tab">
