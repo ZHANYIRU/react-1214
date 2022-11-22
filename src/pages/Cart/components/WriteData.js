1f1ab282e4d4594975bd48c51799ede51d0ae011
YI-RU <game665987@gmail.com>
2022-11-23 01:24:25 +0800

    cart95%

--- src/pages/Cart/components/WriteData.js
+++ src/pages/Cart/components/WriteData.js
@@ -4,7 +4,6 @@ import styled from '../../../styles/cart-scss/writeData.module.scss'
 function WriteData() {
   const [familySelect, setFamilySelect] = useState('')
   const [paySelect, setPaySelect] = useState('')
-
   const [writeUser, setWriteUser] = useState({
     name: '',
     mobile: 0,
@@ -12,9 +11,11 @@ function WriteData() {
     email: '',
     text: '',
   })
-
-  const family = ['宅配', '全家店到店', '郵寄']
-  const pay = ['ATM匯款', '全家取貨付款', 'LINE PAY', '信用卡']
+  const family = ['宅配', '郵寄']
+  const pay = ['ATM匯款', 'LINE PAY', '信用卡']
+  const handle = (e) => {
+    setWriteUser({ ...writeUser, [e.target.name]: e.target.value })
+  }
   return (
     <>
       <div className={styled.writeWrap}>
@@ -91,37 +92,71 @@ function WriteData() {
           <div className={styled.receiveInput}>
             <div>
               <label htmlFor="">姓名</label>
-              <input type="text" name="name" />
+              <input
+                value={writeUser.name}
+                type="text"
+                name="name"
+                onChange={(e) => {
+                  handle(e)
+                }}
+              />
             </div>
             <div>
               <label htmlFor="">手機號碼</label>
-              <input type="text" name="mobile" />
+              <input
+                value={writeUser.mobile}
+                type="text"
+                name="mobile"
+                onChange={(e) => {
+                  handle(e)
+                }}
+              />
             </div>
             <div>
               <label htmlFor="">地址</label>
-              <input type="text" name="address" />
+              <input
+                value={writeUser.address}
+                type="text"
+                name="address"
+                onChange={(e) => {
+                  handle(e)
+                }}
+              />
             </div>
             <div>
               <label htmlFor="">電子郵件</label>
-              <input type="text" name="email" />
+              <input
+                value={writeUser.email}
+                type="text"
+                name="email"
+                onChange={(e) => {
+                  handle(e)
+                }}
+              />
             </div>
             <div>
               <label htmlFor="">備註</label>
-              <textarea id="" rows="5" name="text" />
+              <textarea
+                value={writeUser.text}
+                id=""
+                rows="5"
+                name="text"
+                onChange={(e) => {
+                  handle(e)
+                }}
+              />
             </div>
           </div>
         </div>
-        <div className={styled.catchBox}>
+        {/* <div className={styled.catchBox}>
           <h3>收貨方式</h3>
           <div className={styled.home}>
             <input type="radio" id="home" />
             <label htmlFor="home">宅配地址</label>
-          </div>
-          <div className={styled.family}>
             <input type="radio" id="family" />
             <label htmlFor="family">全家店到店</label>
           </div>
-        </div>
+        </div> */}
         <div className={styled.area}>
           <div className={styled.address}>
             <h3>地址</h3>
