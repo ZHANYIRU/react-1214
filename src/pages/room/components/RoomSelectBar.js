38169f70d7e8029bcfaf11fe519a4dca7cb192fb
wei8549 <kkwei8549@gmail.com>
2022-11-24 17:06:23 +0800

    pull

--- src/pages/room/components/RoomSelectBar.js
+++ src/pages/room/components/RoomSelectBar.js
@@ -1,13 +1,28 @@
-import { useEffect, useState, useRef } from 'react'
+import { useEffect, useState } from 'react'
 import style from '../../../styles/room-scss/roomSelectBar.module.scss'
+// import { DayPicker } from 'react-day-picker'
+// import 'react-day-picker/dist/style.css'
 
 function RoomSelectBar({ detail }) {
+  //房型資料定義
   const roomQTY = detail.room_qty
+  const roomPrice = detail.room_price
+  const roomStart = detail.room_start_date
+  const defaultDate = roomStart && roomStart.split('T', 10)[0]
+  console.log('123',defaultDate);
+  //記錄使用者選擇到的房間數
+  const [qty, setQty] = useState(0)
 
+  //記錄使用者選擇的入住日期
+  const [checkIn, setCheckIn] = useState('')
+
+  //切換是否顯示selectBar
   const [showBar, setShowBar] = useState(false)
 
-  const barHeight = useRef()
+  //切換是否顯示selectCalendar
+  // const [showCalendar, setShowCalendar] = useState(false)
 
+  //判斷使用者滑鼠滾輪往上or往下
   let lastScroll = 0
   const show = () => {
     const windowH = window.innerHeight
@@ -21,29 +36,38 @@ function RoomSelectBar({ detail }) {
     lastScroll = nowscroll
   }
   useEffect(() => {
+    console.log(456)
+    setCheckIn(defaultDate)
     window.addEventListener('scroll', show)
     return () => {
       window.removeEventListener('scroll', show)
     }
-  }, [])
+  }, [defaultDate])
 
   return (
     <>
-      <div
-        className={style.bar}
-        style={{ visibility: showBar && 'hidden' }}
-        ref={barHeight}
-      >
-        <div className={style.name}>南庄小美家民宿</div>
+      <div className={style.bar} style={{ visibility: showBar && 'hidden' }}>
+        <div className={style.name}>{detail.room_name}</div>
         <div className={style.select}>
-          <select>
-            <option>入住日期</option>
-          </select>
-          <select>
-            <option>退房日期</option>
-          </select>
-          <select>
-            <option>床位</option>
+          <label>入住日期</label>
+          <input
+            type="date"
+            value={checkIn}
+            onChange={(e) => {
+              const selDate = e.target.value
+              setCheckIn(selDate)
+            }}
+          />
+          <label>退房日期</label>
+          <input type="date" value="2022-02-11" />
+          <label>床位</label>
+          <select
+            onChange={(e) => {
+              const selectQty = e.target.value
+              setQty(selectQty)
+              console.log(defaultDate)
+            }}
+          >
             {Array(roomQTY)
               .fill(1)
               .map((v, i) => {
@@ -55,7 +79,9 @@ function RoomSelectBar({ detail }) {
               })}
           </select>
         </div>
-        <div className={style.price}>共計：$1,300</div>
+        <div className={style.price}>
+          共計：{qty > 0 ? `${qty * roomPrice}` : 0}元
+        </div>
         <div className={style.add}>加入購物車</div>
       </div>
     </>
