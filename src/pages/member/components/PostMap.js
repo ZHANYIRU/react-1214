11c10c3e136908448748079021a0513e625d9a2d
ChienChihYeh <miroyeh1986@gmail.com>
2022-11-29 17:20:32 +0800

    profileinfo postmap

--- src/pages/member/components/PostMap.js
+++ src/pages/member/components/PostMap.js
@@ -1,18 +1,54 @@
-import { useEffect, useState} from 'react'
+import log from 'eslint-plugin-react/lib/util/log'
+import { useEffect, useState } from 'react'
 import styled from '../../../styles/member-scss/MemberInfo.module.scss'
 
 export default function PostMap(postList) {
+  const [locations, setLocations] = useState([])
 
-const [locations, setLocations] = useState([])
+  useEffect(() => {
+    const uniqueLocations = [
+      ...new Set(postList.postList.map((item) => item.mountain_sid)),
+    ]
+    setLocations(uniqueLocations)
+  }, [postList])
 
-useEffect(()=>{
-  const uniqueLocations = [...new Set(postList.postList.map(item => item.mountain_sid))]
-  setLocations(uniqueLocations)
-}, [postList])
+  function leftPercent(x) {
+    return ((x - 158187) / (349808 - 158187)) * 100
+  }
+
+  function bottomPercent(y) {
+    {
+      console.log(
+        'y=' + y + '| %=' + ((y - 2422925) / (2798819 - 2422925)) * 100
+      )
+    }
+    return ((y - 2422925) / (2798819 - 2422925)) * 100
+  }
 
   return (
     <>
       <div className={styled.postMap}>
+        <img className={styled.magBg} src={'/img/taiwan.png'} alt="taiwanMap" />
+        {postList.postList.map((el, i) => {
+          return (
+            <div
+              className={styled.pinBox}
+              key={i}
+              style={{
+                bottom: `${bottomPercent(el.y)}%`,
+                left: `${leftPercent(el.x)}%`,
+              }}
+            >
+              <i className={`fa-solid fa-location-dot ${styled.pin}`}></i>
+              <p className={styled.pinInfo}>
+                {el.mountain_name}
+                <br />
+                <span style={{ fontSize: '14px' }}>{el.height}m</span>
+              </p>
+            </div>
+          )
+        })}
+        {/* <div className={styled.pin}></div> */}
         <h4>總計地點: {locations.length}</h4>
       </div>
     </>
--- src/pages/member/components/TotalHeight.js
+++ src/pages/member/components/TotalHeight.js
@@ -1,12 +1,26 @@
 import styled from '../../../styles/member-scss/MemberInfo.module.scss'
 
 export default function TotalHeight(totalHeight) {
-
   return (
     <>
       <div className={styled.totalHeight}>
+        <img className={styled.sun} src={'./img/sun_face.png'} alt="sun" />
+        <img className={styled.cloud1} src={'./img/cloud3.png'} alt="cloud" />
+        <img className={styled.cloud2} src={'./img/cloud5.png'} alt="cloud" />
+        <div className={styled.AnimationBox}>
+          <img
+            className={styled.trekker}
+            src={'./img/tozan_trekking_man.png'}
+            alt="trekker"
+          />
+          <img
+            className={styled.mountain}
+            src={'/img/mountain_yama.png'}
+            alt="mountain"
+          />
+        </div>
         <h4>
-          累積海拔:{' '}
+          累積海拔:
           {totalHeight.totalHeight ? totalHeight.totalHeight.height : 0} 公尺
         </h4>
       </div>
