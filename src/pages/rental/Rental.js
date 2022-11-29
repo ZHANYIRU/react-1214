8bffe196a6fa307ddba04c44cad3f6e1a6f0e3f0
bh20511 <bh20511@gmail.com>
2022-11-29 18:43:28 +0800

    準備連結購物車

--- src/pages/rental/Rental.js
+++ src/pages/rental/Rental.js
@@ -5,18 +5,24 @@ import Search from './components/Search'
 import RentalCard from './components/RentalCard'
 import rentalcss from '../../styles/rental-scss/rental.module.scss'
 // import RentalFilter from './components/RentalFilter'
+import { useLocation } from 'react-router-dom'
+import CustomIcons from './components/CustomIcons'
 
 function Rental(props) {
-  let [data, setData] = useState(null)
-  let [count, setCount] = useState(1)
+  const [data, setData] = useState(null)
+  const [count, setCount] = useState(1)
+  const [page, setPage] = useState(1)
+  const [totalPages, setTotalPages] = useState(10)
 
-  const rental_url = 'http://localhost:3001/rental/api'
+  // const rental_url = 'http://localhost:3001/rental/api'
+  const rental_url_new = 'http://localhost:3001/rental/pageApi'
 
   async function getList() {
-    const response = await axios.get(rental_url)
+    const response = await axios.get(rental_url_new + `?page=${page}`)
     console.log(response.data)
     setData(response.data.rows)
-    setCount(response.data.count['COUNT(1)'])
+    setCount(response.data.count)
+    setTotalPages(response.data.totalPages)
   }
 
   const priceOrder = function (text) {
@@ -31,21 +37,34 @@ function Rental(props) {
     })
     setData(newData)
   }
-
+  const timeOrder = function () {
+    const newData = [...data]
+    newData.sort((a, b) => {
+      return Date.parse(a.rental_time) - Date.parse(b.rental_time)
+    })
+    setData(newData)
+  }
   useEffect(() => {
     getList()
-  }, [])
+  }, [page])
 
   return (
     <>
-      <div className={rentalcss.empty}></div>
+      {/* <div className={rentalcss.empty}></div>
+      <button
+        onClick={() => {
+          setPage(page + 1)
+        }}
+      >
+        +
+      </button> */}
 
       {/* <RentalFilter /> */}
       <div className={rentalcss.container}>
         {/* 製作輪播牆 */}
         <Carousel />
         {/* 搜尋元件 */}
-        <Search setData={setData} />
+        <Search setData={setData} setTotalPages={setTotalPages} />
 
         {/* 篩選列表 */}
         <div className={rentalcss.orderShow}>
@@ -55,7 +74,7 @@ function Rental(props) {
           </div>
           <div className={rentalcss.order}>
             <p>一共{count}筆數</p>
-            <p>最新上架</p>
+            <p onClick={() => timeOrder()}>最新上架</p>
             <p>最熱銷</p>
             <p onClick={() => priceOrder('highToLow')}>價格高到低</p>
             <p onClick={() => priceOrder('lowToHigh')}>價格低到高</p>
@@ -68,6 +87,13 @@ function Rental(props) {
             data.map((d) => {
               return <RentalCard data={d} key={d.sid} />
             })}
+        </div>
+        <div className={rentalcss.pageContainer}>
+          <CustomIcons
+            className={rentalcss.pagination}
+            totalPages={totalPages}
+            setPage={setPage}
+          />
         </div>
       </div>
     </>
