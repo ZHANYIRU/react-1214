c0401dd71b9e2ef63a4d14f2f4260dc438d73b96
flybear <goodgood0331@gmail.com>
2022-11-30 22:23:44 +0800

    canvas

--- src/pages/product/Custom.js
+++ src/pages/product/Custom.js
@@ -2,40 +2,60 @@ import React from 'react'
 import styled from '../../styles/product-scss/Custom.module.scss'
 import { useRef } from 'react'
 import { fabric } from 'fabric'
+import { useEffect } from 'react'
+import { useState } from 'react'
 export default function Custom() {
   const customPic = useRef()
-  const customPic2 = useRef()
+  const canvasRef = useRef()
 
-  const canvas = new fabric.Canvas('canvas')
+  const [isDrawing, setIsDrawing] = useState(false)
 
-  fabric.Image.fromURL(
-    'https://www.pakutaso.com/shared/img/thumb/AMEMAN17826009_TP_V.jpg',
-    (img) => {
-      const oImg = img.set({
-        left: 100,
-        top: 100,
-        angle: 15,
-        width: 500,
-        height: 500,
+  useEffect(() => {
+    const initCanvas = (id) => {
+      return new fabric.Canvas(id, {
+        width: 1200,
+        height: 1200,
+        selection: false,
       })
-      canvas.setBackgroundImage(oImg).renderAll()
     }
-  )
+    const setBackground = (url, canvas) => {
+      fabric.Image.fromURL(url, (img) => {
+        canvas.backgroundImage = img
+        canvas.renderAll()
+      })
+    }
+
+    const canvas = initCanvas('canvas')
+
+    let mousePressed = false
+    setBackground(
+      'https://www.pakutaso.com/shared/img/thumb/AMEMAN17826009_TP_V.jpg',
+      canvas
+    )
+    canvas.on('mouse:move', (event) => {
+      if (mousePressed) {
+        const mEvent = event.e
+        const delta = new fabric.Point(mEvent.movementX, mEvent.movementY)
+        canvas.relativePan(delta)
+      }
+    })
+
+    canvas.on('mouse:down', (event) => {
+      mousePressed = true
+    })
+    canvas.on('mouse:up', (event) => {
+      mousePressed = false
+    })
+  }, [])
 
   return (
     <div>
       <div>5</div>
       <div>5</div>
       <div>5</div>
-      <div>5</div>
-      <div>5</div>
-      <div>5</div>
-      <div>5</div>
-
+      <canvas id="canvas" ref={canvasRef} onDrag={() => {}}></canvas>
       <form ref={customPic} encType="multipart/form-data">
-        <div className={styled.canvasArea}>
-          <img src="" ref={customPic2} alt="" />
-        </div>
+        {/* <div className={styled.canvasArea}></div> */}
         <button>上傳圖片</button>
       </form>
     </div>
