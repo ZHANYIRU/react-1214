c0c088a51f3ba60ccbaad58c9af958b26fadc2ea
YI-RU <game665987@gmail.com>
2022-12-07 12:27:14 +0800

    card套件

--- src/pages/Cart/child-pages/Pay.js
+++ src/pages/Cart/child-pages/Pay.js
@@ -2,6 +2,8 @@ import { useRef, useState, useEffect, useContext } from 'react'
 import styled from '../../../styles/cart-scss/Pay.module.scss'
 import ProCartContext from '../../../contexts/ProCartContext'
 import MemberContext from '../../../contexts/MemberContext'
+import Card from 'react-credit-cards'
+import 'react-credit-cards/es/styles-compiled.css'
 import axios from 'axios'
 import dayjs from 'dayjs'
 function subscribe(eventName, listener) {
@@ -20,6 +22,23 @@ function Pay({
   useCoupon,
   writeUser,
 }) {
+  //信用卡套件
+  const [card, setCard] = useState({
+    number: '',
+    name: '',
+    expiry: '',
+    cvc: '',
+    issuer: '',
+    focused: '',
+    formData: null,
+  })
+  const { number, name, expiry, cvc, issuer, focused, formData } = card
+  const handleInputChange = (e) => {
+    setCard({ ...card, [e.target.name]: e.target.value })
+  }
+  const handleInputFocus = (e) => {
+    setCard({ ...card, focused: e.target.name })
+  }
   const { data } = useContext(MemberContext)
   const { pro, ren, room, camp, cartPrice, moneyFormat } =
     useContext(ProCartContext)
@@ -80,13 +99,6 @@ function Pay({
   return (
     <>
       <div className={styled.choose}>
-        {/* <button
-          onClick={() => {
-            setStep(step + 1)
-          }}
-        >
-          下一步test
-        </button> */}
         <button
           onClick={() => {
             setStep(step - 1)
@@ -95,39 +107,130 @@ function Pay({
           重新選擇付款方式
         </button>
       </div>
-
-      <div className={styled.pay}>
-        <p>
-          總金額:{moneyFormat(cartPrice - coupon)}
-          <span>{paid ? '已付款' : '尚未付款'}</span>
-        </p>
-        <button
-          onClick={async () => {
-            testOrder.order.orderId = new Date().getTime()
-            testOrder.order.packages[0].products[0].name = `訂單編號：${testOrder.order.orderId}`
-            const { data } = await axios.post(
-              `http://localhost:3001/order/createOrder`,
-              testOrder
-            )
-            if (data.returnCode === '0000') {
-              setForOk({
-                orderN: testOrder.order.orderId,
-                family: familySelect,
-                pay: paySelect,
-                orderDay: dayjs(testOrder.order.orderId).format('YYYY-MM-DD'),
-                totalPrice: cartPrice - coupon,
-              })
-              newLinePay.current = window.open(
-                `${data?.info.paymentUrl.web}`,
-                'hiking',
-                'height=600,width=800'
+      {paySelect === '信用卡' && (
+        <div className={styled.payCard}>
+          <Card
+            number={number}
+            name={name}
+            expiry={expiry}
+            cvc={cvc}
+            focused={focused}
+          />
+          <form className={styled.form}>
+            <div className="form-group" style={{ marginBottom: '20px' }}>
+              <input
+                type="tel"
+                name="number"
+                className="form-control"
+                placeholder="Card Number"
+                pattern="[\d| ]{16,22}"
+                required
+                maxLength={16}
+                value={card.number}
+                onChange={(e) => {
+                  handleInputChange(e)
+                }}
+                onFocus={(e) => {
+                  handleInputFocus(e)
+                }}
+              />
+              <small>E.g.: 49..., 51..., 36..., 37...</small>
+            </div>
+            <div className="form-group">
+              <input
+                type="text"
+                name="name"
+                value={card.name}
+                className="form-control"
+                placeholder="Name"
+                required
+                onChange={(e) => {
+                  handleInputChange(e)
+                }}
+                onFocus={(e) => {
+                  handleInputFocus(e)
+                }}
+                style={{ marginBottom: '20px' }}
+              />
+            </div>
+            <div className="row">
+              <div className="col-6">
+                <input
+                  type="tel"
+                  name="expiry"
+                  value={card.expiry}
+                  className="form-control"
+                  placeholder="Valid Thru"
+                  pattern="\d\d/\d\d"
+                  required
+                  onChange={(e) => {
+                    handleInputChange(e)
+                  }}
+                  onFocus={(e) => {
+                    handleInputFocus(e)
+                  }}
+                />
+              </div>
+              <div className="col-6">
+                <input
+                  type="tel"
+                  name="cvc"
+                  value={card.cvc}
+                  className="form-control"
+                  placeholder="CVC"
+                  pattern="\d{3,4}"
+                  required
+                  maxLength={3}
+                  onChange={(e) => {
+                    handleInputChange(e)
+                  }}
+                  onFocus={(e) => {
+                    handleInputFocus(e)
+                  }}
+                />
+              </div>
+            </div>
+            <input type="hidden" name="issuer" value={issuer} />
+            <div className="form-actions" style={{ marginTop: '20px' }}>
+              <button className="btn btn-primary btn-block">PAY</button>
+            </div>
+          </form>
+        </div>
+      )}
+      {paySelect === 'LINE PAY' && (
+        <div className={styled.pay}>
+          <p>
+            總金額:{moneyFormat(cartPrice - coupon)}
+            <span>{paid ? '已付款' : '尚未付款'}</span>
+          </p>
+          <button
+            onClick={async () => {
+              testOrder.order.orderId = new Date().getTime()
+              testOrder.order.packages[0].products[0].name = `訂單編號：${testOrder.order.orderId}`
+              const { data } = await axios.post(
+                `http://localhost:3001/order/createOrder`,
+                testOrder
               )
-            }
-          }}
-        >
-          LINE PAY 付款
-        </button>
-      </div>
+              if (data.returnCode === '0000') {
+                setForOk({
+                  orderN: testOrder.order.orderId,
+                  family: familySelect,
+                  pay: paySelect,
+                  orderDay: dayjs(testOrder.order.orderId).format('YYYY-MM-DD'),
+                  totalPrice: cartPrice - coupon,
+                })
+                newLinePay.current = window.open(
+                  `${data?.info.paymentUrl.web}`,
+                  'hiking',
+                  'height=600,width=800'
+                )
+              }
+            }}
+          >
+            LINE PAY 付款
+          </button>
+        </div>
+      )}
     </>
   )
 }
